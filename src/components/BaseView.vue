f54124fed69d7b1323b931f2514149df65ac97fe
Tom <tom.pillokat@googlemail.com>
2020-03-17 19:20:04 +0100

    Done most presets

---
+++ src/components/BaseView.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container><slot></slot></v-container>
+</template>
+
+<script lang="ts">
+import { createComponent } from "@vue/composition-api";
+export default createComponent({
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<style scoped></style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,17 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
+// Types
+import { VuetifyPreset } from "vuetify/types/services/presets";
+
+const options = {
+  theme: {
+    dark: true,
+    default: "dark",
+    disable: false
+  }
+};
+
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify(options);
